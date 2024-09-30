7ebdee819eb890010e51695f1df53e232b4c6e9c
Debajit <debajit.dey90@gmail.com>
2024-09-30 16:59:10 +0530

    Parcel installed and NPM environment created

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello From React App!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
